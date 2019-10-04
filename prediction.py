066d383a3938500d18572254556e8b7eeec52ece
mirceta <kristijan.mirceta@gmail.com>
2019-10-04 22:54:02 +0200

    averages by dataset

--- prediction.py
+++ prediction.py
@@ -305,7 +305,16 @@ if __name__ == '__main__':
         #     print(minangle)
 
 
+    # PREDICTIONS TO LATEX TABLES
     mainlines = ''
+
+    avghoughbynames = []
+    stdhoughbynames = []
+    avgderivbynames = []
+    stdderivbynames = []
+    avgairplanebynames = []
+    stdairplanebynames = []
+
     for name in names:
 
         if name not in predictions.keys():
@@ -352,14 +361,55 @@ if __name__ == '__main__':
 
 
         a = name.split('_')
+
+        avgairplanebynames.append(avgpred)
+        stdairplanebynames.append(stddevpred)
+        avghoughbynames.append(np.min(np.array(avghough)))
+        stdhoughbynames.append(np.min(np.array(stdhough)))
+        avgderivbynames.append(np.min(np.array(avgderiv)))
+        stdderivbynames.append(np.min(np.array(stdderiv)))
+
+        # main table
+        # line = a[0] + '\\_' + a[1] + ' & '
+        # line += format(avgpred) + ' & '
+        # line += format(stddevpred) + ' & '
+        # line += format(np.min(np.array(avghough))) + ' & '
+        # line += format(np.min(np.array(stdhough))) + ' & '
+        # line += format(np.min(np.array(avgderiv))) + ' & '
+        # line += format(np.min(np.array(stdderiv))) + '\\\\ \n'
+        # line += '\\hline'
+        # print(line)
+
+
+        # hough table by bmpsize
         line = a[0] + '\\_' + a[1] + ' & '
-        line += format(avgpred) + ' & '
-        line += format(stddevpred) + ' & '
-        line += format(np.min(np.array(avghough))) + ' & '
-        line += format(np.min(np.array(stdhough))) + ' & '
-        line += format(np.min(np.array(avgderiv))) + ' & '
-        line += format(np.min(np.array(stdderiv))) + '\\\\ \n'
+        for i in range(len(avghough)):
+            line += format(avghough[i]) + ' & '
+        line = line[:-3] + '\\\\ \n'
         line += '\\hline'
         print(line)
+
+        # deriv table by bmpsize
+        # line = a[0] + '\\_' + a[1] + ' & '
+        # for i in range(len(avgderiv)):
+        #     line += format(avgderiv[i]) + ' & '
+        # line = line[:-3] + '\\\\ \n'
+        # line += '\\hline'
+        # print(line)
+
+    # aggregate by names
+    def avgbynames(array):
+        return '\\textbf{' + format(np.average(np.array(array))) + "}"
+    print('AVG BY NAMES')
+    line = " & "
+    line += avgbynames(avgairplanebynames) + " & "
+    line += avgbynames(stdairplanebynames) + " & "
+    line += avgbynames(avghoughbynames) + " & "
+    line += avgbynames(stdhoughbynames) + " & "
+    line += avgbynames(avgderivbynames) + " & "
+    line += avgbynames(stdderivbynames) + "\\\\ \n"
+
+    print(line)
+
 
 
